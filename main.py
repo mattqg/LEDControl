0f46d4b9101dddbea025c747ad4ca677f54d6b84
mattqg <matthew.q.gothard@vanderbilt.edu>
2023-02-04 21:28:55 -0500

    added import

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from matrix.bindings.python.rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 64
+options.cols = 64
+options.chain_length = 2
+options.parallel = 1
+# If you have an Adafruit HAT: 'adafruit-hat'
+options.hardware_mapping = 'regular'
+
 # tdb
